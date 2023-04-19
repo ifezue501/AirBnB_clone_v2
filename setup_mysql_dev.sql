28a7653c419045f5b5ca9cea99f9c58d84e576a8
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-04-19 04:57:17 -0700

    Fixing the setup mysql dev file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,15 +5,14 @@
 --   Grants SELECT privilege for hbnb_dev on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    PASSWORD('hbnb_dev_pwd');
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   PASSWORD('hbnb_dev_pwd');
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   PASSWORD('hbnb_dev_pwd');
+
+-- Create a user with username 'hbnb_dev' and 
+-- password 'hbnb_dev_pwd' on localhost
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Granting all privileges tot the new user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- Granting the SELECT privileges for the user hbnb_dev in Performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Flushing privileges.
 FLUSH PRIVILEGES;
